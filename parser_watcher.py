ac031f2a2c6ef7b7244679b310063a6815fb1bdd
root <root@localhost.localdomain>
2021-06-22 20:58:01 +0200

    new test

--- parser_watcher.py
+++ parser_watcher.py
@@ -8,7 +8,7 @@ from prettyprinter import pprint
 # python3  parser_watcher.py   'davidboukari@gmail.com' 'srv,Code,Timestamp\nmyserver5,CODEA2,2021-05-11T12:37:42, 90, (90%)\nmyserver4,CODEA2,2021-05-11T12:39:47, 85, (85%)\nmyserver3,CODEA2,2021-05-11T12:33:48, 87, (87%)\nmyserver10,CODEA2,2021-05-11T12:31:46, 93, (93%)\nmysever8,CODEA2,2021-05-11T12:39:43, 130, (130%)\n' 'Alert CPU' 'server_mail.csv' 'OPERATION_SYSTEM-CPU' acknowledge
 
 
-# python3  parser_watcher.py   'davidboukari@gmail.com' 'srv,Code,Timestamp\nmyserver5,CODEA2,2021-05-11T12:37:42, /bigdata/disk1, 90, (90%)\nmyserver4,CODEA2,2021-05-11T12:39:47, /bigdata/disk1, 85, (85%)\nmyserver3,CODEA2,2021-05-11T12:33:48, /ftp1, ,87, (87%)\nmyserver10,CODEA2,2021-05-11T12:31:46, /nfs/nfs1, 93, (93%)\nmysever8,CODEA2,2021-05-11T12:39:43, /rootvg, 130, (130%)\n' 'Alert CPU' 'server_mail.csv' 'OPERATION_SYSTEM-CPU' acknowledge
+# python3  parser_watcher.py   'davidboukari@gmail.com' 'srv,Code,Timestamp\nmyserver5,CODEA2,2021-05-11T12:37:42, /bigdata/disk1, 90, (90%)\nmyserver4,CODEA2,2021-05-11T12:39:47, /bigdata/disk1, 85, (85%)\nmyserver3,CODEA2,2021-05-11T12:33:48, /ftp1, ,87, (87%)\nmyserver10,CODEA2,2021-05-11T12:31:46, /nfs/nfs1, 93, (93%)\nmyserver8,CODEA2,2021-05-11T12:39:43, /rootvg, 130, (130%)\n' 'Alert CPU' 'server_mail.csv' 'OPERATION_SYSTEM-DISK' acknowledge
 
 def get_dico_from_csv_file(csv_file):
   print(f'-----> get_dico_from_csv_file({csv_file})')
